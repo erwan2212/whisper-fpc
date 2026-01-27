8a3158e915c4f3d13d5e4cc0d531b304c5bf9ea0
erwan2212 <erwan2212@gmail.com>
2026-01-27 19:43:57 +0100

    Adjust n_max_text_ctx for improved grammar
    Updated n_max_text_ctx to allow context memory for Whisper.

--- whisper.pas
+++ whisper.pas
@@ -340,6 +340,7 @@ begin
   // 2. On initialise manuellement les valeurs par défaut standards de Whisper
   result.strategy := longint(WHISPER_SAMPLING_BEAM_SEARCH);
   result.beam_search_beam_size := 3;
+  result.beam_search_patience := -1.0; //(valeur par défaut pour "auto"). Si c'est à 0.0 (via FillChar), le Beam Search pourrait s'arrêter trop tôt.
 
   result.temperature := 0.0;
   result.temperature_inc := 0.2;
@@ -381,7 +382,7 @@ begin
   result.temperature := 0.0;
   result.temperature_inc := 0.0;
 
-  result.n_max_text_ctx := 0; //radical. Cela signifie que Whisper n'a aucune mémoire de ce qu'il a dit juste avant. C'est très rapide, mais la grammaire peut en souffrir.
+  result.n_max_text_ctx := 50; //0 = radical. Cela signifie que Whisper n'a aucune mémoire de ce qu'il a dit juste avant. C'est très rapide, mais la grammaire peut en souffrir.
 
   result.no_speech_thold := 0.6;
   result.logprob_thold := -1.0;
